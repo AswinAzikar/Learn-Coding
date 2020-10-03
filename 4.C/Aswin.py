86a7a3ee6862c8fc76f2d5161b5e23af50fe3d84
AswinAzikar <65263396+AswinAzikar@users.noreply.github.com>
2020-10-03 16:22:53 +0530

    Create Aswin.py

---
+++ 4.C/Aswin.py
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   :Aswin\n");
+     printf("Age    : 19\n");
+     printf("Username : AswinAzikar\n");
+     return(0);
+  }
+© 2020 GitHub, Inc.
